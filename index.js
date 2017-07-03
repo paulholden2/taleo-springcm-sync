9baa5ffb12305157e1760fed47e86664f5c09c7e
Paul Holden <pholden@stria.com>
2017-07-03 16:30:29 -0700

    Update with some optimizations
    - Updated Taleo and SpringCM SDK dependencies
    - Activities are globbed first, then split amongst child processes. Child
      processes further divide the workload into separate async call chains with
      an "allowance" property, such that the sum of all process allowances is
      < 20 (the maximum number of auth tokens Taleo will allocate to any given
      account). We assume one key is reserved (for debugging, we predefine a
      token to reduce the number of auth calls made).

--- index.js
+++ index.js
@@ -1,147 +1,152 @@
-const fs = require('fs');
+const os = require('os');
+const child = require('child_process');
 // Require local library version, these other libraries aren't stable yet
 const Taleo = require('../taleo-nodejs-sdk');
-const SpringCM = require('../springcm-nodejs-sdk');
 const async = require('async');
 
-var locations = {};
+var locationLookup = {};
+var employeeLookup = {};
 
 function getLocations(callback) {
 	Taleo.location.all((err, locs) => {
 		if (err) {
-			return callbac(err);
+			return callback(err);
 		}
 
 		locs.forEach((loc) => {
-			locations[loc.id] = loc;
+			locationLookup[loc.id] = loc;
 		});
 
 		callback(null);
 	});
 }
 
-function iterateEmployees(employees, callback) {
-	async.eachSeries(employees, (emp, next) => {
-		Taleo.employee.packets(emp, (err, packets) => {
-			if (err) {
-				console.log(err);
-				return next(null);
-			}
-
-			console.log(`${emp.id} ${emp.firstName} ${emp.lastName}` + (emp.location ? ` - ${locations[emp.location].city}` : ' - Unknown Location'));
-
-			SpringCM.folder.get(`/Taleo Packet Uploads/${emp.id} ${emp.firstName} ${emp.lastName}`, (err, fld) => {
-				if (err) {
-					console.log(err);
-					return next(null);
-				}
-
-				async.eachSeries(packets, (packet, next) => {
-					Taleo.packet.activities(packet, (err, activities) => {
-						async.eachSeries(activities, (actv, next) => {
-							var docname = `${actv.id} ${actv.title}.pdf`;
-
-							if (!actv.signed()) {
-								console.log(`Skipping ${actv.id}, not complete`);
-								return next(null);
-							}
-
-							SpringCM.document.path(`/Taleo Packet Uploads/${emp.id} ${emp.firstName} ${emp.lastName}/${docname}`, (err, doc) => {
-								if (!doc) {
-									Taleo.activity.download(actv, `${__dirname}/${docname}`, (err) => {
-										if (err) {
-											console.log(err);
-											next(null);
-										} else {
-											SpringCM.folder.upload(fld, `${__dirname}/${docname}`, null, (err) => {
-												if (err) {
-													console.log(err);
-												} else {
-													console.log(`Uploaded /Taleo Packet Uploads/${emp.id} ${emp.firstName} ${emp.lastName}/${docname}`);
-													fs.unlinkSync(`${__dirname}/${docname}`);
-												}
-
-												next(null);
-											});
-										}
-									});
-								} else {
-									console.log(`/Taleo Packet Uploads/${emp.id} ${emp.firstName} ${emp.lastName}/${docname} already exists`);
-									next(null);
-								}
-							});
-						}, (err) => {
-							next(err);
-						});
-					});
-				}, (err) => {
-					next(err);
-				});
-			});
-		});
-	}, (err) => {
-		if (err) {
-			console.log(err);
-		} else {
-			callback(null);
-		}
-	});
-}
-
-function iteratePages(pages, callback) {
-	async.eachSeries(pages, (page, next) => {
-		page.read((err, employees) => {
-			iterateEmployees(employees, (err) => {
-				next(null);
-			});
-		});
-	}, (err) => {
-		if (err) {
-			console.log(err);
-		} else {
-			callback(null);
-		}
-	});
-}
-
 async.waterfall([
-	// SpringCM auth
-	(callback) => {
-		SpringCM.auth.uatna11(process.env.SPRINGCM_CLIENT_ID, process.env.SPRINGCM_CLIENT_SECRET, (err, token) => {
-			callback(err);
-		});
-	},
 	// Taleo dispatcher service
 	(callback) => {
 		Taleo.dispatcher.serviceURL((err, url) => {
 			callback(err);
 		});
 	},
+	// Get locations (also creates location lookup)
 	(callback) => {
 		getLocations(callback);
 	},
 	// Get employee pages
 	(callback) => {
-		Taleo.employee.pages(100, (err, pages) => {
+		Taleo.employee.pages(200, (err, pages) => {
 			callback(err, pages);
 		});
 	},
-	// Iterate through pages
+	// Combine pages into a single list of employees
 	(pages, callback) => {
-		iteratePages(pages, (err) => {
-			if (err) {
-				callback(err);
-			} else {
-				console.log('Completed employee page');
+		var employees = [];
+
+		// Compile a list of all employees
+		async.eachSeries(pages, (page, next) => {
+			Taleo.page.read(page, (err, res) => {
+				employees = employees.concat(res);
+				next(err);
+			});
+		}, (err) => {
+			callback(err, employees);
+		});
+	},
+	// Create employee lookup
+	(employees, callback) => {
+		employees.forEach((emp) => {
+			employeeLookup[emp.id] = emp;
+		});
+
+		callback(null, employees);
+	},
+	// Get a list of activities for all packets for each employee
+	(employees, callback) => {
+		// Map each employee to an array of activities
+		async.mapLimit(employees, 18, (employee, callback) => {
+			Taleo.employee.packets(employee, (err, packets) => {
+				// List of signed activities for this employee
+				var activities = [];
+
+				async.eachSeries(packets, (packet, next) => {
+					Taleo.packet.activities(packet, (err, res) => {
+						// Filter out unsigned/incomplete activity forms
+						res.forEach((actv) => {
+							if (Taleo.activity.signed(actv)) {
+								activities.push(actv);
+							}
+						});
+
+						console.log(`${activities.length} form${activities.length === 1 ? '' : 's'} in ${packets.length} packet${packets.length === 1 ? '' : 's'} found for ${employee.id} - ${employee.firstName} ${employee.lastName}`);
+
+						next(err);
+					});
+				}, (err) => {
+					callback(err, activities);
+				});
+			});
+		}, (err, lists) => {
+			callback(err, lists);
+		});
+	},
+	// Flatten the 2D array of activities
+	(lists, callback) => {
+		var activities = [];
+
+		lists.forEach((list) => {
+			// May be undefined if employee had no packets
+			if (list) {
+				list.forEach((item) => {
+					activities = activities.concat(item);
+				});
 			}
 		});
+
+		console.log(`${activities.length} total activities to sync to SpringCM`);
+
+		callback(null, activities);
+	},
+	// Assign child processes a portion of the activities to sync to SpringCM
+	(activities, callback) => {
+		// CPU count
+		var cpus = os.cpus().length;
+		// Pages per process. Splice at this index
+		var per = Math.ceil(activities.length / cpus);
+
+		console.log(`${cpus} CPUs`);
+		console.log(`${activities.length} activities`);
+		console.log(`${per} activities per process`);
+
+		// Split up activities to upload amongst processes
+		for (var i = 0; i < cpus; ++i) {
+			// If all pages are assigned, stop
+			if (i * per >= activities.length) {
+				break;
+			}
+
+			var c = child.fork('./sync');
+			var cpid = c.pid;
+			// How many asynchronous tracks each process may use
+			// 20 max Taleo tokens, reserve one
+			var allowance = Math.floor(19 / cpus);
+
+			var from = i * per;
+			var to = Math.min((i + 1) * per - 1, activities.length - 1);
+
+			console.log(`Spawned child process ${cpid}, assigning ` + (from === to ? `activity ${from}` : `activities ${from} - ${to} (${to - from + 1} total)`));
+			c.send(JSON.stringify({
+				allowance: allowance,
+				employeeLookup: employeeLookup,
+				locationLookup: locationLookup,
+				activities: activities.slice(from, to + 1)
+			}));
+		}
+
+		callback(null);
 	}
 ], (err) => {
 	if (err) {
 		console.log(err);
 		process.exit(1);
-	} else {
-		console.log('done');
-		process.exit(0);
 	}
 });
---
+++ sync.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const async = require('async');
+const Taleo = require('../taleo-nodejs-sdk');
+const SpringCM = require('../springcm-nodejs-sdk');
+
+const tag = `[${process.pid}] `;
+
+function iterateActivities(employeeLookup, locationLookup, folder, activities, callback) {
+	async.eachSeries(activities, (actv, next) => {
+		var emp = employeeLookup[actv.employeeID];
+		var loc = emp.location && locationLookup[emp.location];
+		var docname = `${emp.id} ${emp.firstName} ${emp.lastName} - ${actv.id} ${actv.title}.pdf`;
+
+		SpringCM.document.path(`/Taleo Packet Uploads/${docname}`, (err, doc) => {
+			if (!doc) {
+				Taleo.activity.download(actv, `${__dirname}/${docname}`, (err) => {
+					if (err) {
+						console.log(tag + err);
+						next(null);
+					} else {
+						SpringCM.folder.upload(folder, `${__dirname}/${docname}`, null, (err) => {
+							if (err) {
+								console.log(tag + err);
+							} else {
+								console.log(tag + `Uploaded: /Taleo Packet Uploads/${docname}`);
+								fs.unlinkSync(`${__dirname}/${docname}`);
+							}
+
+							next(null);
+						});
+					}
+				});
+			} else {
+				console.log(tag + `Already exists: /Taleo Packet Uploads/${docname}`);
+				next(null);
+			}
+		});
+	}, (err) => {
+		callback(err);
+	});
+}
+
+process.on('message', (msg) => {
+	msg = JSON.parse(msg);
+
+	var locationLookup = msg.locationLookup;
+	var employeeLookup = msg.employeeLookup;
+	var activities = msg.activities;
+	var allowance = msg.allowance;
+	var folder = null;
+
+	console.log(tag + `Received ${activities.length} activities, allowance of ${allowance}`);
+
+	async.waterfall([
+		// Taleo dispatcher service
+		(callback) => {
+			Taleo.dispatcher.serviceURL((err, url) => {
+				callback(err);
+			});
+		},
+		// SpringCM auth
+		(callback) => {
+			SpringCM.auth.uatna11(process.env.SPRINGCM_CLIENT_ID, process.env.SPRINGCM_CLIENT_SECRET, (err, token) => {
+				callback(err);
+			});
+		},
+		(callback) => {
+			SpringCM.folder.get('/Taleo Packet Uploads', (err, fld) => {
+				folder = fld;
+				callback(err);
+			});
+		},
+		// Split activities to sync into 1 chunk per allowance
+		// Allowance is allocated such that the program never
+		// attempts to allocate more than 20 tokens from Taleo
+		(callback) => {
+			var len = Math.ceil(activities.length / allowance) + 1;
+
+			async.times(allowance, (n, next) => {
+				var from = n * len;
+				var to = Math.min(from + len, activities.length);
+
+				console.log(tag + `Section ${from} - ${to - 1}`);
+
+				iterateActivities(employeeLookup, locationLookup, folder, activities.slice(from, to), (err) => {
+					next(err);
+				});
+			}, (err) => {
+				callback(err);
+			});
+		}
+	], (err) => {
+		process.send(err);
+		process.exit(err ? 1 : 0);
+	});
+});
